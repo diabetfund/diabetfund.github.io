eae331aadd30ab7a1316a1503ba2ee5d477340e8
Sergey Kasperovich <82015158+sergeks@users.noreply.github.com>
2022-11-18 10:17:36 +0200

    ersioning

--- source/generator/Program.cs
+++ source/generator/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text.Json;
 
-var version = 21;
+var version = 22;
 var rootPath = Environment.CurrentDirectory.Split("source")[0];
 
 var (projects, news, partners, thanks, slides_) = 
