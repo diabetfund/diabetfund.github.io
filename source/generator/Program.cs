a3b8305d28665c89b406fcf9d38acddeaa98ab57
Sergey Kasperovich <82015158+sergeks@users.noreply.github.com>
2022-11-22 10:56:33 +0200

    mail form

--- source/generator/Program.cs
+++ source/generator/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text.Json;
 
-var version = 24;
+var version = 25;
 var rootPath = Environment.CurrentDirectory.Split("source")[0];
 
 var (projects, news, partners, thanks, slides_) = 
