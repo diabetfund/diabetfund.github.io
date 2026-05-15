c1bcad1058c63dafb60ed69cbe6b0c541653b59c
alexeninng <121716849+alexeninng@users.noreply.github.com>
2026-05-15 12:01:23 +0300

    Update script.js

--- source/script.js
+++ source/script.js
@@ -67,9 +67,9 @@ const lib = {
     sendLiqpay(sign, data, isNewWindow = false) {
         let form = document.createElement("form");
         if (!sign || sign == "null")
-            sign = Lang.trans("8FCafqMc//6iKe9wB+eqZWs3FPc=", "TVNsm5bs8KyxZhkpsexBFHb8Mb8=");
+            sign = Lang.trans("whUgouW+4esoSpOMJXlkgAqKCI8=", "P5TSqLhuxFW/iogdY3q+KmWtplc=");
         if (!data || data == "null")
-            data = Lang.trans("eyJhY3Rpb24iOiJwYXlkb25hdGUiLCJhbW91bnQiOiI1MDAiLCJjdXJyZW5jeSI6IlVBSCIsImRlc2NyaXB0aW9uIjoiRG9uYXRlIHRvIHRoZSBmdW5kIiwicmVzdWx0X3VybCI6Imh0dHBzOlwvXC9kaWFiZXQuZnVuZFwvZW4iLCJsYW5ndWFnZSI6ImVuIiwidmVyc2lvbiI6IjMiLCJwdWJsaWNfa2V5IjoiaTMwNzg0ODE1MTQzIn0=", "eyJhY3Rpb24iOiJwYXlkb25hdGUiLCJhbW91bnQiOiI1MDAiLCJjdXJyZW5jeSI6IlVBSCIsImRlc2NyaXB0aW9uIjoiXHUwNDFmXHUwNDNlXHUwNDM2XHUwNDM1XHUwNDQwXHUwNDQyXHUwNDMyXHUwNDQzXHUwNDMyXHUwNDMwXHUwNDQyXHUwNDM4IFx1MDQzMiBcdTA0NDRcdTA0M2VcdTA0M2RcdTA0MzQiLCJyZXN1bHRfdXJsIjoiaHR0cHM6XC9cL2RpYWJldC5mdW5kXC91YSIsImxhbmd1YWdlIjoidWsiLCJ2ZXJzaW9uIjoiMyIsInB1YmxpY19rZXkiOiJpMzA3ODQ4MTUxNDMifQ==");
+            data = Lang.trans("eyJhY3Rpb24iOiJwYXlEb25hdGUiLCJhbW91bnQiOiI1MDAiLCJjdXJyZW5jeSI6IlVBSCIsImRlc2NyaXB0aW9uIjoi0J/QvtC20LXRgNGC0LLRg9Cy0LDRgtC4INCyINGE0L7QvdC0IiwicmVzdWx0X3VybCI6Imh0dHBzOi8vZGlhYmV0LmZ1bmQvdWEiLCJsYW5ndWFnZSI6InVrIiwidmVyc2lvbiI6IjMifQ==", "eyJhY3Rpb24iOiJwYXlEb25hdGUiLCJhbW91bnQiOiIxMDAiLCJjdXJyZW5jeSI6IlVTRCIsImRlc2NyaXB0aW9uIjoiRG9uYXRlIHRvIHRoZSBmdW5kIiwicmVzdWx0X3VybCI6Imh0dHBzOi8vZGlhYmV0LmZ1bmQvZW4iLCJsYW5ndWFnZSI6ImVuIiwidmVyc2lvbiI6IjMifQ==");
         form.method = "POST";
         form.action = "https://www.liqpay.ua/api/3/checkout";
         form.innerHTML = `<input type="hidden" name="data" value="${data}"/>
